9bb4f12883a06f0319d0cd4d9055dc9846ccc25b
Fitlomov <lomovc@gmail.com>
2019-12-04 17:43:26 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
 import java.util.Arrays;
-
+import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
-        String[] Array1 = {
-            "test", "service", "test2", "dev", "test3", "service2", "test4", "prod", "test5"
-        };
-        String[] Array2 = {
-            "test5", "service2", "test3", "prod", "dev"
-        };
+        String[] Array1 = new String[9];
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Введите элементы первого массива");
+        for (int i = 0; i < Array1.length; i++) {
+            Array1[i] = input.nextLine();
+        }
+        System.out.println("Введите размерность и элементы второго массива");
+        int size = input.nextInt();
+        String[] Array2 = new String[size];
+        for (int i = 0; i < Array2.length; i++) {
+            Array2[i] = input.nextLine();
+        }
+
         System.out.println(Arrays.asList(Array1));
         System.out.println(Arrays.asList(Array2));
 
@@ -15,8 +23,8 @@ public class Main{
 
         for (int counter= 0; counter < 9; counter++) {
           Boolean counterChar = false;
-            for (int counter2 = 0; counter2 < 5; counter2++) {
-                if (Array1[counter] == Array2[counter2]) {
+            for (int counter2 = 0; counter2 < Array2.length; counter2++) {
+                if (Array1[counter].equals(Array2[counter2])) {
                     counterChar = true;
                     break;
                 }
@@ -24,7 +32,7 @@ public class Main{
                     counterChar = false;
                 }
             }
-          if(counterChar == true){
+          if(counterChar.equals(true)){
               String_out = String_out + "1";
           }
           else {
